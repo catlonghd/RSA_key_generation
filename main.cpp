0cf730dd69640e6cb2af246696c289c9e8330e35
Tran Hai Dang <75677317+catlonghd@users.noreply.github.com>
2022-12-09 10:55:42 +0700

    Update main.cpp
    update

--- main.cpp
+++ main.cpp
@@ -1,27 +1,53 @@
 #include "BigInt.h"
 #include "RSA.h"
 #include <iostream>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 
 int main() {
+    int n;
+    if (argc == 1) {
+        cout << "1. 512 bits\n";
+        cout << "2. 1024 bits\n";
+        cout << "3. 2048 bits\n";
+        cout << "RSA Algorithm" << endl;
+        cout << "Chon do dai khoa (VD: Nhap 512): ";
+        cin >> n;
+    }
+    else if (argc == 2) {
+        n = atoi(argv[1]);
+    }
+    else {
+        cout << "Qua nhieu tham so\n";
+        return 0;
+    }
+    n = n / 32;
+    
     cout << "RSA Algorithm" << endl;
-    RSABigInt rsa(390);
+    RSABigInt rsa(100);
     cout << "Random Prime Number Generation" << endl;
+    
+    auto start = high_resolution_clock::now();
+    
+    srand(unsigned(time(NULL)));
 
-    //struct timespec tstart={0,0}, tend={0,0};
-    //clock_gettime(CLOCK_MONOTONIC, &tstart);
-
-    BigInt primeNumberP(390), primeNumberQ(390);
-    rsa.primeGeneration(primeNumberP, 512/32/2);
+    BigInt primeNumberP(100), primeNumberQ(100);
+    rsa.primeGeneration(primeNumberP, n/2);
     cout << "\nprimeNumber, p=\n";
     primeNumberP.showDigits();
 
-    rsa.primeGeneration(primeNumberQ, 512/32/2);
+    rsa.primeGeneration(primeNumberQ, n/2);
     cout << "\nprimeNumber, q=\n";
     primeNumberQ.showDigits();
 
     rsa.init(primeNumberP, primeNumberQ);
+    
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<seconds>(stop - start);
+    cout << "\n\nTime taken by function: "
+        << duration.count() << " seconds" << endl;
     //rsa.printD();
     return 0;
 }
